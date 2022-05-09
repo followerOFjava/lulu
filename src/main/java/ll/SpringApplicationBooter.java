e4dc07d104a9278216eb5b8e822bb7c2eac069e3
followerofjava <18248974745@163.com>
2022-05-09 19:13:33 +0800

    项目可以启动

---
+++ src/main/java/ll/SpringApplicationBooter.java
@@ -0,0 +1,11 @@
+package ll;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringApplicationBooter {
+    public static void main(String[] args) {
+        SpringApplication.run(SpringApplicationBooter.class,args);
+    }
+}
